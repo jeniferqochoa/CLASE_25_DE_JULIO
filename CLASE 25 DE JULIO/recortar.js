1fb9f904ca9969cc0b45a373d5005d3235f57ac1
jeniferqochoa <141057448+jeniferqochoa@users.noreply.github.com>
2023-08-28 07:56:29 -0500

    Add files via upload
    CLASE 25 DE JULIO JENIFER QUIROZ OCHOA

---
+++ CLASE 25 DE JULIO/recortar.js
@@ -0,0 +1,9 @@
+function recortar (cantidadLetras, palabras){
+    const word = palabras.map((numPalabra)=>{
+     return numPalabra.slice(0, cantidadLetras)
+     
+ })
+ console.log(word) 
+ }
+ 
+ console.log(recortar(3,["hola", "elefante"]))
